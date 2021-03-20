4cbff597f01275192f3cdb84cc60e0211f2b34fc
ppelegrinoamaral <paulamaralspi@hotmail.com>
2021-03-20 12:44:13 -0300

    teste feito até agora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { InputComponent } from './components/input/input.component';
+import { MatInputModule } from '@angular/material/input';
+
+import { MatButtonModule } from '@angular/material/button';
+import { ButtonComponent } from './components/button/button.component';
+
+import { PatientComponent } from './components/patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    InputComponent,
+    ButtonComponent,
+    PatientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
